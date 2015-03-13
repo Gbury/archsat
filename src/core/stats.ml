f0e52621c4bf754f028f9d3803ab8273898532a9
Guillaume Bury <guillaume.bury@gmail.com>
2015-03-13 15:16:49 +0100

    Forgot to add new files

---
+++ src/core/stats.ml
@@ -0,0 +1,53 @@
+
+let log_section = Util.Section.make "stats"
+let log i fmt = Util.debug ~section:log_section i fmt
+
+(* Statistics record & manipulations *)
+type t = {
+  mutable cur_round : int;
+  instanciations : (int * int) Vector.t;
+}
+
+let init_round state i =
+  state.cur_round <- i;
+  Vector.set state.instanciations i (0, 0)
+
+let print_state state =
+  let s = state.cur_round in
+  let (i, r) = Vector.get state.instanciations s in
+  log 0 "Round %d : instanciations %d (remaining : %d)" s i r
+
+
+(* State record *)
+let state = {
+  cur_round = 0;
+  instanciations = Vector.make 100 (0, 0);
+  }
+
+let () = init_round state 0 (* Initialization for round 0 *)
+
+(* Exported functions *)
+let inst_done () =
+  let s = state.cur_round in
+  let (i, r) = Vector.get state.instanciations s in
+  Vector.set state.instanciations s (i + 1, r)
+
+let inst_remaining r =
+  let s = state.cur_round in
+  let (i, _) = Vector.get state.instanciations s in
+  Vector.set state.instanciations s (i, r)
+
+(* Print stats and prep for next round *)
+let clock () =
+  print_state state;
+  let i = state.cur_round + 1 in
+  init_round state i
+
+;;
+Dispatcher.(register (
+  mk_ext
+    ~descr:"Handles delayed printing of statistics for each round of solving"
+    ~prio:~-10 ~if_sat:clock
+    (new_id ()) "stats"
+))
+
---
+++ src/core/stats.mli
@@ -0,0 +1,8 @@
+
+val inst_done : unit -> unit
+(** Adds one to the number of instanciatiosn done on the current round. *)
+
+val inst_remaining : int -> unit
+(** Sets the number of instanciations remaining to do this round. *)
+
+
