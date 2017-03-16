8793a48d8ffd9dd24bffc079c31052b5fdb01fe0
Guillaume Bury <guillaume.bury@gmail.com>
2017-03-16 23:11:40 +0100

    Reset style before printing regular messages

--- src/util/util.ml
+++ src/util/util.ml
@@ -15,7 +15,7 @@ let printf format =
   if !debug then
     Logs.log ~section:Section.root ~lvl:Level.log format
   else
-    Format.fprintf Format.std_formatter format
+    CCFormat.with_colorf "reset" Format.std_formatter format
 
 (* Logging functions *)
 (* ************************************************************************ *)
